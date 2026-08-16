45fab12ccace161ca3a0ce31505e9d74d9040b7c
Royvil Domingo <royvildomingo53@gmail.com>
2026-08-16 15:26:24 +0800

    addition of footers and headers

---
+++ pages/choice.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Which</title>
+</head>
+<body>
+    <?php
+    require_once 'header.php';
+    ?>
+    <div class="container">
+        <h1>Which</h1>
+        <p>Choose one of the following options:</p>
+        
+        <div>
+            <button>Option 1</button>
+        </div>
+
+        <div>
+            <button>Option 2</button>
+        </div>
+    </div>
+
+    <?php
+    require_once 'footer.php';
+    ?>
+</body>
+</html>
